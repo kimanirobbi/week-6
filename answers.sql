0753839b072b828ee7f8075f0d04a48d3c5e3440
kimanirobbi <kimanirobbi3@gmail.com>
2025-09-06 16:30:26 +0300

    Complete SQL assignment queries

---
+++ answers.sql
@@ -0,0 +1,34 @@
+/* Question1 */
+SELECT 
+    employees.firstName,
+    employees.lastName,
+    employees.email,
+    employees.officeCode
+FROM 
+    employees
+INNER JOIN 
+    offices ON employees.officeCode = offices.officeCode;
+
+/* Question2 */
+SELECT
+    products.productName,
+    products.productVendor,
+    products.productLine
+FROM
+    products
+LEFT JOIN
+    productlines ON products.productLine = productlines.productLine;
+
+/* Question3 */
+SELECT
+    orders.orderDate,
+    orders.shippedDate,
+    orders.status,
+    orders.customerNumber
+FROM
+    customers
+RIGHT JOIN
+    orders ON customers.customerNumber = orders.customerNumber
+ORDER BY
+    orders.orderDate
+LIMIT 10;
